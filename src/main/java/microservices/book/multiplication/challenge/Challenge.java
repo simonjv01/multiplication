611d66979f5baef994fcf29d68a069860401a68f
Simon Vargas <sjvargas01@gmail.com>
2023-01-12 14:26:12 -0500

    updated classes

--- src/main/java/microservices/book/multiplication/challenge/Challenge.java
+++ src/main/java/microservices/book/multiplication/challenge/Challenge.java
@@ -1,2 +1,15 @@
-package microservices.book.multiplication.challenge;public class Challenge {
+package microservices.book.multiplication.challenge;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Challenge {
+    private int factorA;
+    private int factorB;
 }
