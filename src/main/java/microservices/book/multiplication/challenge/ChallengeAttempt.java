d0d677098903a66dd40d07d28f7a0345580c2808
Simon Vargas <sjvargas01@gmail.com>
2023-01-12 14:42:18 -0500

    updated class files

--- src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
@@ -1,2 +1,19 @@
-package microservices.book.multiplication.challenge;public class ChallengeAttempt {
+package microservices.book.multiplication.challenge;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class ChallengeAttempt {
+    private Long id;
+    private Long userId;
+    private int factorA;
+    private int factorB;
+    private int resultAttempt;
+    private boolean correct;
 }
--- src/main/java/microservices/book/multiplication/user/User.java
+++ src/main/java/microservices/book/multiplication/user/User.java
@@ -1,2 +1,15 @@
-package microservices.book.multiplication.user;public class User {
+package microservices.book.multiplication.user;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String alias;
 }
